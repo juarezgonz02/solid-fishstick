cd1bbd497543a6ea134021ddde3e21cd423d7cde
MarcelaPortillo <00079419@uca.edu.sv>
2023-05-30 12:45:55 -0600

    Adding son entity and repository

--- src/main/java/com/escruadronlobo/devs/parcial2/models/entities/Song.java
+++ src/main/java/com/escruadronlobo/devs/parcial2/models/entities/Song.java
@@ -1,5 +1,6 @@
 package com.escruadronlobo.devs.parcial2.models.entities;
 
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -9,13 +10,17 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "song")
@@ -23,18 +28,18 @@ public class Song{
     @Id
     @Column(name = "code")
     @GeneratedValue(strategy = GenerationType.AUTO)
-	private UUID code;
+    private UUID code;
 
     @Column(name = "title")
     private String title;
 
-    @Column(name = "duration")
-    private Integer duration;
-
     @OneToMany(mappedBy = "songCode", fetch = FetchType.LAZY)
     @JsonIgnore
     @ToString.Exclude
-	private List<SongXPlaylist> playlists;
+    private List<SongXPlaylist> playlists;
+
+    @Column(name = "duration")
+    private Integer duration;
 
     public Song(String title, Integer duration) {
         this.title = title;
