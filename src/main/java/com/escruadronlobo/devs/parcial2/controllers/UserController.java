b8493d9a5e1b0f23a94f07b831ad0cc307d7761c
Sofia Galvez <00154517@uca.edu.sv>
2023-05-30 16:15:03 -0600

    User Controller
    User Controller

--- src/main/java/com/escruadronlobo/devs/parcial2/controllers/UserController.java
+++ src/main/java/com/escruadronlobo/devs/parcial2/controllers/UserController.java
@@ -25,78 +25,38 @@ import com.escruadronlobo.devs.parcial2.service.UserServiceInterface;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 public class UserController {
-	
-	@Autowired
-	private UserServiceInterface userService;
-	
-	@GetMapping("/all")
-	public ResponseEntity<?> findAllUsers(){
-		List<User> users = userService.findAll();
-		return new ResponseEntity<>(
-				users, HttpStatus.OK
-				);
-	}
-	
-	
-	@GetMapping("/{code}")
-	public ResponseEntity<?> findUserByCode(@PathVariable("code") String code){
-		try {
-	        User foundUser = userService.findOneById(code);
-	        if (foundUser == null) {
-	            return new ResponseEntity<>(new MessageDTO("User not found"), HttpStatus.NOT_FOUND);
-	        }
-	        return new ResponseEntity<>(foundUser, HttpStatus.OK);
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        return new ResponseEntity<>(new MessageDTO("Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
-	    }
-	}
-	
-	@GetMapping("/search")
-	public ResponseEntity<?> findOneUserByUsername(@RequestParam("username") String username){
-		try {
-            List<User> users = userService.findOneByUsername(username);
-            return new ResponseEntity<>(users, HttpStatus.OK);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-	}
-	
-	@DeleteMapping("/delete/{code}")
-	public ResponseEntity<?> deleteUserByCode(@PathVariable("code") @Valid String code){
-		try {
-			User foundUser = userService.findOneById(code);
-			if (foundUser == null) {
-	            return new ResponseEntity<>(new MessageDTO("User not found"), HttpStatus.NOT_FOUND);
-	        }
-			
-			userService.deleteOneById(code);
-			return new ResponseEntity<>(new MessageDTO("User deleted"), HttpStatus.OK);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-	        return new ResponseEntity<>(new MessageDTO("Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-	}
-	
-	@PutMapping("/update/{code}")
-	public ResponseEntity<?> updatePlayList(@PathVariable("code") String code, @RequestBody UserDTO user) {
-	    try {
-	        User existingUser = userService.findOneById(code);
-	        if (existingUser == null) {
-	            return new ResponseEntity<>(new MessageDTO("User not found"), HttpStatus.NOT_FOUND);
-	        }
-	        existingUser.setUsername(user.getUsername());
-	        existingUser.setEmail(user.getEmail());
 
-	        userService.update(code, user);
-	        return new ResponseEntity<>(new MessageDTO("User updated"), HttpStatus.OK);
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        return new ResponseEntity<>(new MessageDTO("Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
-	    }
+	private final UserService userService;
+
+	@Autowired
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+
+	@GetMapping
+	public List<User> getAllUsers() {
+		return userService.getAllUsers();
+	}
+
+	@GetMapping("/{code}")
+	public User getUserByCode(@PathVariable UUID code) {
+		return userService.getUserByCode(code);
+	}
+
+	@PostMapping
+	public void createUser(@RequestBody User user) {
+		userService.createUser(user);
+	}
+
+	@PutMapping("/{code}")
+	public void updateUser(@PathVariable UUID code, @RequestBody User user) {
+		userService.updateUser(code, user);
+	}
+
+	@DeleteMapping("/{code}")
+	public void deleteUser(@PathVariable UUID code) {
+		userService.deleteUser(code);
 	}
 }
